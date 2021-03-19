32d1d34b30b0c6d0e86697f1d7ddd64c92062549
Rojhan Paydar <rojhanpaydar@gmail.com>
2021-03-19 14:45:38 -0400

    add header, logo, and global setup

---
+++ src/components/common/Header/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="wrapper">
+      <div>
+        <h1>Location Picker</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
