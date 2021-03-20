8c73d3afb2fd8b24d549f0e8df2b02c26e141dc0
Katie King <kingofkaties@gmail.com>
2021-03-20 11:41:24 -0400

    Add Layout component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Header from "./components/common/Header/Main";
+import Layout from "./components/Layout/Main";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Layout>
+        <p>
+          Hi Rojhan! This is super cool!
+        </p>
+      </Layout>
     </div>
   );
 }
