0aabd4f1aa25b129fbcd90283cef45e001cb2f03
Rojhan Paydar <rojhanpaydar@gmail.com>
2021-03-20 10:52:55 -0400

    adjust image pathing to header and added nav to header component and removed divs and replace with header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
 import Header from "./components/common/Header/Main";
-import Images from "./images/logo.png";
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <img src={Images} alt="logo" />
-      </nav>
       <Header />
     </div>
   );
