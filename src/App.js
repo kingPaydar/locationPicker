e292585a5d452bcfa464e5a4f6ac49809287c269
Rojhan Paydar <rojhanpaydar@gmail.com>
2021-04-04 12:58:09 -0400

    initialize MapBox API and render results to page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import { useEffect, useState } from "react";
+import { React, useState } from "react";
 import "./App.css";
 import Header from "./components/common/Header/Main";
 import Layout from "./components/Layout/Main";
+import axios from "axios";
 
 function App() {
-
+  const [searchTerm, setSearchTerm] = useState();
+  const [response, setResponse] = useState(null);
+  const [title, setTitle] = useState();
   const callApi = () => {
-    axios.get('mapbox.whatever...', {
-      params: {
-        BBOX: [-79.54069836850799, 43.589463188226745, -79.27123066211873, 43.79940068737784],
-        query: 'coffee'
-      }
-    })
-      .then
-  }
+    axios
+      .get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchTerm}.json?bbox=-79.54069836850799,43.589463188226745,-79.27123066211873,43.79940068737784&access_token=pk.eyJ1Ijoia2lhb29oIiwiYSI6ImNrbWh5ZzNzcDBjOGcycGw4bjNhYmJwYTkifQ.xTnMP341DkwkE2qPrwPJRQ`
+      )
+      .then((response) => {
+        setResponse(response);
+        const currentTitle = response.data.features.map((item, index) => {
+          return <li key={`place-${index}`}>{item.place_name}</li>;
+        });
+        console.log(currentTitle);
+        setTitle(currentTitle);
+      });
+  };
 
   return (
     <div className="App">
       <Header />
       <Layout>
-        <p>
-          Hi Rojhan! This is super cool!
-        </p>
+        <p>Hi Rojhan! This is super cool!</p>
+        <input
+          onChange={(event) => setSearchTerm(event.target.value)}
+          type="text"
+          placeholder="Search.."
+        />
         <button onClick={callApi}>call API</button>
+        <ul>{response && title}</ul>
       </Layout>
     </div>
   );
