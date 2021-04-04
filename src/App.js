943a11a1a11caea2c170fc3ac7d62f7272e87531
Katie King <kingofkaties@gmail.com>
2021-04-04 11:24:11 -0400

    Test API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/common/Header/Main";
 import Layout from "./components/Layout/Main";
 
 function App() {
+
+  const callApi = () => {
+    axios.get('mapbox.whatever...', {
+      params: {
+        BBOX: [-79.54069836850799, 43.589463188226745, -79.27123066211873, 43.79940068737784],
+        query: 'coffee'
+      }
+    })
+      .then
+  }
+
   return (
     <div className="App">
       <Header />
@@ -10,6 +22,7 @@ function App() {
         <p>
           Hi Rojhan! This is super cool!
         </p>
+        <button onClick={callApi}>call API</button>
       </Layout>
     </div>
   );
